0a012399676d3bf3a64c9aeef7586bc255473c63
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-08-19 15:20:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,7 +25,7 @@ class Dog
     end
 
   def self.new_from_db(row)
-    new_dog = self.new( id: row[0], name: row[1], breed: [2])
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
     new_dog
   end 
 
